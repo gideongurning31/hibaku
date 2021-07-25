2988a93f1979002a0b2c2441212506f44fc134c3
gideongurning31 <gideon.gurning@gmail.com>
2021-07-25 21:19:11 +0700

    Utilize navigation from material sidenav component

--- src/app/menu/menu-routing.module.ts
+++ src/app/menu/menu-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './pages/home.component';
+import { RegProdusenComponent } from './pages/reg-produsen.component';
+import { RegPenerimaComponent } from './pages/reg-penerima.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'registrasi-produsen',
+        component: RegProdusenComponent,
+      },
+      {
+        path: 'registrasi-penerima',
+        component: RegPenerimaComponent,
+      },
+    ],
   },
 ];
 
--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -8,11 +8,11 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MenuRoutingModule } from './menu-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './pages/home.component';
-import { RegPenerimaComponent } from './pages/reg-penerima.component';
 import { RegProdusenComponent } from './pages/reg-produsen.component';
+import { RegPenerimaComponent } from './pages/reg-penerima.component';
 
 @NgModule({
-  declarations: [DashboardComponent, HomeComponent, RegPenerimaComponent, RegProdusenComponent],
+  declarations: [DashboardComponent, HomeComponent, RegProdusenComponent, RegPenerimaComponent],
   imports: [CommonModule, FormsModule, ReactiveFormsModule, MatSidenavModule, MatToolbarModule, MatButtonModule, MatDividerModule, MenuRoutingModule],
   providers: [],
 })
