2a352965eb52d3dd0c6c0fdb2973b806adb8194b
gideongurning31 <gideon.gurning@gmail.com>
2021-07-26 00:43:04 +0700

    Create form to register user

--- src/app/registrasi/registrasi.component.ts
+++ src/app/registrasi/registrasi.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
@@ -6,6 +7,42 @@ import { MatDialogRef } from '@angular/material/dialog';
   templateUrl: './registrasi.component.html',
   styleUrls: ['../login/login.component.scss', './registrasi.component.scss'],
 })
-export class RegistrasiComponent {
+export class RegistrasiComponent implements OnInit {
+  registerForm: FormGroup;
+
   constructor(public dialogRef: MatDialogRef<RegistrasiComponent>) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.registerForm = new FormGroup({
+      accountType: new FormControl(null, Validators.required),
+      nik: new FormControl(null, Validators.required),
+      firstName: new FormControl(null, Validators.required),
+      lastName: new FormControl(null),
+      birthPlace: new FormControl(null, Validators.required),
+      birthDate: new FormControl(null, Validators.required),
+      address1: new FormControl(null, Validators.required),
+      address2: new FormControl(null, Validators.required),
+      address3: new FormControl(null, Validators.required),
+      city: new FormControl(null, Validators.required),
+      zipCode: new FormControl(null, Validators.required),
+    });
+  }
+
+  submit() {
+    console.info('===', this.registerForm.value);
+  }
+
+  close() {
+    this.registerForm.reset();
+    this.dialogRef.close();
+  }
+}
+
+enum AccountType {
+  TYPE1 = 'PRODUSEN',
+  TYPE2 = 'PENERIMA',
 }
