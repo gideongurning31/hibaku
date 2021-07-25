e8dc3599bc550b8ee49df4a0bbdaa2fcf1629c18
gideongurning31 <gideon.gurning@gmail.com>
2021-07-25 19:38:22 +0700

    Create side navigation menu component & initial dummy menu

--- src/app/menu/dashboard/dashboard.component.ts
+++ src/app/menu/dashboard/dashboard.component.ts
@@ -1,7 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  template: '<h1>DASHBOARD<h1>',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+  profile: UserProfile;
+  navMenu: Array<Menu>;
+  showFiller: boolean;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.showFiller = false;
+    this.initUserProfile();
+    this.initMenu();
+  }
+
+  initUserProfile() {
+    this.profile = {
+      id: localStorage.getItem('userId'),
+      name: localStorage.getItem('displayName'),
+      role: localStorage.getItem('roleName'),
+    };
+  }
+
+  initMenu() {
+    this.navMenu = [
+      { name: 'Menu 1', icon: 'fa-circle', route: '' },
+      { name: 'Menu 2', icon: 'fa-circle', route: '' },
+      { name: 'Menu 3', icon: 'fa-circle', route: '' },
+      { name: 'Menu 4', icon: 'fa-circle', route: '' },
+    ];
+  }
+
+  logout() {}
+}
+
+interface UserProfile {
+  id: string;
+  name: string;
+  role: string;
+}
+
+interface Menu {
+  name: string;
+  icon: string;
+  route: string;
+}
--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { MenuRoutingModule } from './menu-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './pages/home.component';
+import { RegPenerimaComponent } from './pages/reg-penerima.component';
+import { RegProdusenComponent } from './pages/reg-produsen.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, MenuRoutingModule],
+  declarations: [DashboardComponent, HomeComponent, RegPenerimaComponent, RegProdusenComponent],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatSidenavModule, MatToolbarModule, MatButtonModule, MatDividerModule, MenuRoutingModule],
   providers: [],
 })
 export class MenuModule {}
