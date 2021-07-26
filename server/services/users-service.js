6067f4b3d65bdf03140ac0d971d8bc0a1902542f
gideongurning31 <gideon.gurning@gmail.com>
2021-07-26 19:55:38 +0700

    Create service to register user's account

--- server/services/users-service.js
+++ server/services/users-service.js
@@ -1,7 +1,10 @@
 const Model = require('../models/index');
 const UsersModel = Model.Users;
+const UsersInfoModel = Model.UsersInfo;
 const RolesModel = Model.Roles;
 const bcrypt = require('bcrypt');
+const moment = require('moment');
+const BadRequestError = require('../errors/bad-request-error');
 const saltRounds = parseInt(process.env.BCRYPT_SALT);
 
 class UsersService {
@@ -9,6 +12,10 @@ class UsersService {
 
   fetchUsers() {
     return UsersModel.findAll({ where: { verified: true } });
+  }
+
+  fetchUsersDetails() {
+    return UsersInfoModel.findAll();
   }
 
   findByUsername(username) {
@@ -22,6 +29,34 @@ class UsersService {
     const salt = bcrypt.genSaltSync(saltRounds);
     payload.pass = bcrypt.hashSync(payload.pass, salt);
     return UsersModel.create(payload);
+  }
+
+  registerUser(user) {
+    delete user.userId;
+    user.birthDate = new Date(moment(user.birthDate, 'YYYYMMDD'));
+    return UsersInfoModel.findOne({ where: { nik: user.nik } }).then((result) => {
+      if (result) throw new BadRequestError(`NIK ${user.nik} sudah terdaftar.`);
+      return UsersInfoModel.create(user);
+    });
+  }
+
+  createAccount(payload) {
+    let updatedUser;
+    return UsersInfoModel.findOne({ where: { nik: payload.nik } })
+      .then((info) => {
+        if (!info) throw new BadRequestError(`NIK ${payload.nik} belum didaftarkan.`);
+        updatedUser = info.dataValues;
+        return this.createUser({
+          userId: payload.userId,
+          pass: payload.pass,
+          displayName: info.lastName ? `${info.firstName} ${info.lastName}` : info.firstName,
+          verified: true
+        });
+      })
+      .then(() => {
+        updatedUser.userId = payload.userId;
+        return UsersInfoModel.update(updatedUser, { where: { nik: payload.nik } });
+      });
   }
 
   fetchRoles() {
