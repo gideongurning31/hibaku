5c0e0f05d7e350f303263f8dd404a7c1a4d62a6a
gideongurning31 <gideon.gurning@gmail.com>
2021-07-31 11:46:51 +0700

    Register the pagination component

---
+++ src/app/utils/component/pagination/pagination.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'hibaku-pagination',
+  templateUrl: 'pagination.component.html',
+  styleUrls: ['pagination.component.scss'],
+})
+export class PaginationComponent implements OnInit {
+  constructor() {}
+
+  @Output() setPage: EventEmitter<Paging> = new EventEmitter();
+  @Input() paging: Paging;
+  rows: number;
+  pages: Array<number> = [];
+
+  ngOnInit() {
+    this.rows = this.paging.rowPerPage;
+    this.generateButtons();
+  }
+
+  generateButtons() {
+    this.pages = [];
+    this.paging.totalPage = Math.ceil(this.paging.totalRow / this.rows);
+    for (let i = 0; i < this.paging.totalPage; i++) {
+      this.pages.push(i + 1);
+    }
+  }
+
+  onPageClick(page: number) {
+    if (page > 0 && page !== this.paging.page) {
+      this.paging.page = page;
+      this.setPage.emit(this.paging);
+    }
+  }
+
+  onSetRows() {
+    this.paging.rowPerPage = this.rows;
+    this.paging.page = 1;
+    this.setPage.emit(this.paging);
+    this.generateButtons();
+  }
+}
+
+interface ApiPaging {
+  page: number;
+  rowPerPage: number;
+  totalRow: number;
+}
+
+export interface Paging extends ApiPaging {
+  totalPage: number;
+}
--- src/app/utils/utils.module.ts
+++ src/app/utils/utils.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { LoginGuard, RouteGuard } from './service/route-guard.service';
@@ -11,15 +12,16 @@ import { BaseFormComponent } from './component/base-form.component';
 import { NotFoundComponent } from './component/not-found.component';
 import { AlertDialogComponent } from './component/alert-dialog.component';
 import { SpinnerCloakComponent } from './component/spinner-cloak/spinner-cloak.component';
+import { PaginationComponent } from './component/pagination/pagination.component';
 import { FullAddressPipe } from './pipe/full-address.pipe';
 import { NoWhitespacePipe } from './pipe/no-whitespace.pipe';
 import { OnlyNumericPipe } from './pipe/only-numeric.pipe';
 
 @NgModule({
-  declarations: [BaseFormComponent, NotFoundComponent, AlertDialogComponent, SpinnerCloakComponent, FullAddressPipe, NoWhitespacePipe, OnlyNumericPipe],
+  imports: [CommonModule, HttpClientModule, FormsModule, MatDialogModule, MatProgressSpinnerModule],
+  declarations: [BaseFormComponent, NotFoundComponent, AlertDialogComponent, SpinnerCloakComponent, PaginationComponent, FullAddressPipe, NoWhitespacePipe, OnlyNumericPipe],
   entryComponents: [],
-  imports: [CommonModule, HttpClientModule, MatDialogModule, MatProgressSpinnerModule],
   providers: [RouteGuard, LoginGuard, HttpUtilService, AuthService, SpinnerCloakService],
-  exports: [BaseFormComponent, SpinnerCloakComponent, FullAddressPipe, NoWhitespacePipe, OnlyNumericPipe],
+  exports: [BaseFormComponent, SpinnerCloakComponent, PaginationComponent, FullAddressPipe, NoWhitespacePipe, OnlyNumericPipe],
 })
 export class UtilsModule {}
