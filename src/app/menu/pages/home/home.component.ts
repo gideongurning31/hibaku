bd00c49c3032e9df3a34937da704b642eebadae9
gideongurning31 <gideon.gurning@gmail.com>
2021-08-01 16:02:14 +0700

    Create landing page

--- src/app/menu/pages/home/home.component.ts
+++ src/app/menu/pages/home/home.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'hibaku-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  menu: Array<MainIcon>;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.menu = [
+      { title: 'Transaksi Hibah dengan Optimasi', name: 'Transaksi', route: 'transaksi' },
+      { title: 'Pengaturan Komoditas HIBAKU', name: 'Komoditas', route: 'komoditas' },
+      { title: 'Manajemen Pasokan & Permintaan', name: 'Supply / Demand', route: 'supply-demand' },
+      { title: 'Manajemen & Registrasi Akun User', name: 'Registrasi User', route: 'registrasi-penerima' },
+    ];
+  }
+
+  navigate(route: string) {
+    this.router.navigate([route]);
+  }
+}
+
+interface MainIcon {
+  title: string;
+  name: string;
+  route: string;
+}
